29d6911598c3eec20a087bdc3d78ae3dbce03c91
Priyank Bhambu <pbhambu07@gmail.com>
2025-11-16 02:23:57 -0800

    Landing page and map updates
    Added more fish to landing page, updated map style

--- script.js
+++ script.js
@@ -628,16 +628,15 @@ window.addEventListener('resize', () => {
   map.invalidateSize();
 });
 
-// Dark ocean-themed baselayer (CartoDB Dark Matter)
+// High-quality dark ocean-themed baselayer
 L.tileLayer(
-  'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+  'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}',
   {
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    maxZoom: 6,
+      '&copy; <a href="https://www.esri.com/">Esri</a>, HERE, Garmin, FAO, NOAA, USGS',
+    maxZoom: 8,
     minZoom: 2,
-    noWrap: true,
-    subdomains: 'abcd'
+    noWrap: true
   }
 ).addTo(map);
 
